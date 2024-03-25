0b761debaadf1c5adccb30ca31c2954b5c9b0468
GabrielAlvesDS <gabriel.cavalcante.alves@gmail.com>
2024-03-25 14:17:33 -0300

    first commit

---
+++ carprice_pro/CarPrice_Pro.py
@@ -0,0 +1,96 @@
+import pickle
+import numpy  as np
+import pandas as pd
+
+class CarPrice_Pro:
+    
+    def __init__( self ):
+        self.AnoFabricacao_scaler     = pickle.load(open(r'parameter\AnoFabricacao_scaler.pkl', 'rb'))
+        self.AnoModelo_scaler         = pickle.load(open(r'parameter\AnoModelo_scaler.pkl', 'rb'))
+        self.KM_scaler                = pickle.load(open(r'parameter\KM_scaler.pkl', 'rb'))
+        self.df_encoding              = pickle.load(open(r'parameter\df_encoding.pkl', 'rb'))
+        self.Estado_scaler            = pickle.load(open(r'parameter\Estado_scaler.pkl', 'rb'))
+        self.Cidade_scaler            = pickle.load(open(r'parameter\Cidade_scaler.pkl', 'rb'))
+        self.Cambio_scaler            = pickle.load(open(r'parameter\Cambio_scaler.pkl', 'rb'))
+        self.Cor_scaler               = pickle.load(open(r'parameter\Cor_scaler.pkl', 'rb'))
+        self.UnicoDono_scaler         = pickle.load(open(r'parameter\UnicoDono_scaler.pkl', 'rb'))
+        self.IPVAPago_scaler          = pickle.load(open(r'parameter\IPVAPago_scaler.pkl', 'rb'))
+        self.Licenciado_scaler        = pickle.load(open(r'parameter\Licenciado_scaler.pkl', 'rb'))
+        self.Blindado_scaler          = pickle.load(open(r'parameter\Blindado_scaler.pkl', 'rb'))
+        self.TipoVendedor_scaler      = pickle.load(open(r'parameter\TipoVendedor_scaler.pkl', 'rb'))        
+
+    def remove_outliers(self, df, column_name, lower_multiplier=1, upper_multiplier=1):
+        Q1 = df[column_name].quantile(0.25)
+        Q3 = df[column_name].quantile(0.75)
+        IQR = Q3 - Q1
+
+        # Identificar outliers usando o critério IQR
+        outliers = ~((df[column_name] < (Q1 - lower_multiplier * IQR)) | (df[column_name] > (Q3 + upper_multiplier * IQR)))
+
+        # Filtrar os dados removendo os outliers
+        df_filtered = df[outliers]
+
+        # Resetar o índice para garantir um índice padrão
+        df_filtered.reset_index(drop=True, inplace=True)
+
+        return df_filtered
+    
+    def data_cleaning(self, df):
+        # Rename Columns
+        cols = ['Marca', 'Modelo', 'Versao', 'AnoFabricacao', 'AnoModelo', 'Estado', 'Cidade', 'KM', 'Cambio', 'Cor', 'UnicoDono', 'IPVAPago', 'Licenciado', 'Blindado', 'TipoVendedor']
+        df = df[cols]
+
+        # Filtering outliers in KM
+        df = self.remove_outliers(df, 'KM')
+
+        # Filtering AnoModelo
+        df = self.remove_outliers(df, 'AnoModelo')
+
+        # Filtering AnoFabricacao
+        df = self.remove_outliers(df, 'AnoFabricacao')
+
+        # Filtering Brands
+        # Remover Marcas com menos de 1% do dataset
+        df_aux = df['Marca'].value_counts(normalize = True).reset_index()
+        df_aux = df_aux[df_aux['proportion'] >= 0.01]
+        top_marcas = df_aux['Marca']
+        df = df[df['Marca'].isin(top_marcas)]
+
+        return df
+    
+    def data_preparation(self, df1):
+        ## 5.1. Rescaling
+        df1['AnoFabricacao'] = self.AnoFabricacao_scaler.transform(df1[['AnoFabricacao']].values.reshape(-1, 1))
+        df1['AnoModelo']     = self.AnoModelo_scaler.transform(df1[['AnoModelo']].values.reshape(-1, 1))
+        df1['KM']            = self.KM_scaler.transform(df1[['KM']].values.reshape(-1, 1))
+
+        ## 5.2. Encoding
+
+        # Mapeamento para 'Versao'
+        mapeamento_versao = self.df_encoding.set_index(['Marca', 'Modelo', 'Versao'])['Versao_encoded'].to_dict()
+        df1['Versao'] = df1[['Marca', 'Modelo', 'Versao']].apply(lambda x: mapeamento_versao.get(tuple(x), x[2]), axis=1)
+
+        mapeamento_modelo = self.df_encoding.set_index(['Marca', 'Modelo'])['Modelo_encoded'].to_dict()
+        df1['Modelo'] = df1[['Marca', 'Modelo']].apply(lambda x: mapeamento_modelo.get(tuple(x), x[1]), axis=1)
+
+        mapeamento_marca = self.df_encoding.set_index('Marca')['Marca_encoded'].to_dict()
+        df1['Marca'] = df1['Marca'].map(mapeamento_marca)
+
+
+        df1['Estado']        = self.Estado_scaler.transform(df1['Estado'].values)
+        df1['Cidade']        = self.Cidade_scaler.transform(df1['Cidade'].values)
+        df1['Cambio']        = self.Cambio_scaler.transform(df1['Cambio'].values)
+        df1['Blindado']      = self.Blindado_scaler.transform(df1['Blindado'].values)
+        df1['Cor']           = self.Cor_scaler.transform(df1['Cor'].values)
+        df1['UnicoDono']     = self.UnicoDono_scaler.transform(df1['UnicoDono'].values)
+        df1['IPVAPago']      = self.IPVAPago_scaler.transform(df1['IPVAPago'].values)
+        df1['Licenciado']    = self.Licenciado_scaler.transform(df1['Licenciado'].values)
+        df1['TipoVendedor']  = self.TipoVendedor_scaler.transform(df1['TipoVendedor'].values)
+
+        return df1
+
+    def data_prediction(self, model, df2):
+        y_pred = model.predict(df2)
+        y_pred = np.expm1(y_pred).round(2)
+
+        return y_pred
---
+++ handler.py
@@ -0,0 +1,38 @@
+import os
+import pickle
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify
+from carprice_pro.CarPrice_Pro import CarPrice_Pro
+
+model = pickle.load(open(r'model\lr_test_model.pkl', 'rb'))
+
+# Criar uma instância da classe Flask
+app = Flask(__name__)
+
+# Definir as rotas da API
+@app.route('/', methods=['GET', 'POST'])
+
+def carprice_predict():
+
+    try:
+        data = request.get_json()
+
+        # df para coletar os dados recebidos
+        df = pd.DataFrame(data)
+
+        # Processar os dados
+        pipeline = CarPrice_Pro()
+        df1 = pipeline.data_cleaning(df)
+        df2 = pipeline.data_preparation(df1)
+        predicted_price = pipeline.data_prediction(model, df2)
+
+        return jsonify({'predicted_price': predicted_price.tolist()})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+  
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run( host='0.0.0.0', port=port, debug=True )
